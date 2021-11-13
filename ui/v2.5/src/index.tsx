cf4950fd62cb1aec5f59488d12fa17ea043eb3d1
exl3m <94209538+exl3m@users.noreply.github.com>
2021-11-13 13:25:22 -0600

    fix pwa (https://github.com/facebook/create-react-app/issues/10032#issuecomment-723433217)

--- ui/v2.5/src/index.tsx
+++ ui/v2.5/src/index.tsx
@@ -6,7 +6,7 @@ import { App } from "./App";
 import { getClient } from "./core/StashService";
 import { getPlatformURL, getBaseURL } from "./core/createClient";
 import "./index.scss";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <>
@@ -22,5 +22,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
